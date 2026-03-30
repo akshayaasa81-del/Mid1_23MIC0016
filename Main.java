7eac35eb9904fedba0569711f90366247295aa8f
akshayaasa81-del <akshayaasa81@gmail.com>
2026-03-30 16:15:39 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-package MidTest;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Student Name: Akshayaa.S.A");
         System.out.println("Register Number: 23MIC0016");
-        System.out.println("Message: Jenkins Build Successful");
+        System.out.println("Message: Jenkins Build Successfully");
     }
 }
